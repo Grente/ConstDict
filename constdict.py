95dacc0708f480eccb299daa5acf21aa168d1628
Grenter <1819847843@qq.com>
2022-03-26 17:52:10 +0800

    Update constdict.py
    renew

--- constdict.py
+++ constdict.py
@@ -1,6 +1,9 @@
 # -*- coding: utf-8 -*-
 
+
 class ConstDict(object):
+    __slots__ = []
+
     def __init__(self, dic):
         for key, val in dic.iteritems():
             setattr(self, key, val)
@@ -9,47 +12,63 @@ class ConstDict(object):
         return iter(self.__slots__)
 
     def __getitem__(self, item):
-        return getattr(self, item, None)
+        if not isinstance(item, str):
+            return None
+        return getattr(self, item)
 
     def __setitem__(self, key, value):
         return setattr(self, key, value)
-    
+
     def __contains__(self, item):
-        try:
-            return getattr(self, item, False)
-        except:
-            return False
-        
-    def get(self, key, default):
-        return getattr(self, key, default)
-
-    def iteritems(self):
-        return ((key, getattr(self, key, None)) for key in self.__slots__)
-
-    def items(self):
-        return [(key, getattr(self, key, None)) for key in self.__slots__]
-
-    def iterkeys(self):
-        return iter(self.__slots__)
-
-    def itervalues(self):
-        return (getattr(self, key, None) for key in self.__slots__)
-
-    def keys(self):
-        return self.__slots__
-
-    def values(self):
-        return [getattr(self, key, None) for key in self.__slots__]
+        return hasattr(self, item)
     
-    def update(self, dic):
-        """
-        const dict noly support exist keys update
-        """
-        for key, val in dic.iteritems():
-            if getattr(self, key, False):
-                setattr(self, key, val)
-        raise NotImplementedError("ConstDictBase not support update")
+    def __repr__(self):
+        return self.__str__()
     
     def __str__(self):
         return str(dict(self.items()))
 
+    def get(self, key, default=None):
+        return getattr(self, key, default)
+    
+    def update(self, dic):
+        for key, val in dic.iteritems():
+            if key in self.__slots__:
+                setattr(self, key, val)
+    
+    def clear(self):
+        for key in self.__slots__:
+            if hasattr(self, key):
+                delattr(self, key)
+    
+    def setdefault(self, key, val):
+        if hasattr(self, key):
+            return getattr(self, key)
+        else:
+            setattr(self, key, val)
+            return val
+    
+    def pop(self, key, default=None):
+        if hasattr(self, key):
+            val = getattr(self, key)
+            delattr(self, key)
+            return val
+        return default
+        
+    def iteritems(self):
+        return iter(self.items())
+
+    def items(self):
+        return [(key, getattr(self, key, None)) for key in self.__slots__ if hasattr(self, key)]
+
+    def iterkeys(self):
+        return iter(self.keys())
+
+    def itervalues(self):
+        return iter(self.values())
+
+    def keys(self):
+        return [key for key in self.__slots__ if hasattr(self, key)]
+
+    def values(self):
+        return [getattr(self, key, None) for key in self.__slots__ if hasattr(self, key)]
